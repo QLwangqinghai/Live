606a2bc7b5f23efb9077013fe2ed13882e32305b
vector <dev_vector@qq.com>
2020-06-06 19:16:36 +0800

    no message

--- RtmKit/Demo/ViewController.swift
+++ RtmKit/Demo/ViewController.swift
@@ -11,14 +11,26 @@ import RtmKit
 
 class ViewController: UIViewController {
 
-    public let rtmKit: RtmKit = RtmKit()
+    public let rtmKit: RtmKit = RtmKit(account: "007")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        print("\(self.rtmKit)")
         
+        self.rtmKit.login(token: nil)
         
+        let image = UIImage(named: "058f99f392446c526b9ea278faf4bed3")!
+        let data = image.jpegData(compressionQuality: 1.0)!
+        
+        let filePath = "\(NSHomeDirectory())/Library/a.png"
+        try! data.write(to: URL(fileURLWithPath: filePath))
+        
+        print("data.size \(data.count)")
+        
+        self.rtmKit.fileManager.create(filePath: filePath)
+
     }
 
 
--- RtmKit/RtmKit/RtmKit.swift
+++ RtmKit/RtmKit/RtmKit.swift
@@ -27,15 +27,43 @@ public final class RtmKit: NSObject, AgoraRtmDelegate {
     }
     
     private let api: AgoraRtmKit
-    public dynamic private(set) var state: ConnectState
-    public override init() {
+    public let fileManager: RtmFileManager
+    
+    public dynamic private(set) var state: ConnectState {
+        didSet(old) {
+            print("state changed from:\(old) to:\(self.state)")
+        }
+    }
+    
+    public private(set) var account: String
+    public private(set) var token: String?
+
+    public init(account: String) {
+        self.account = account
         let api = AgoraRtmKit(appId: "f5d079b7a3eb43b28bd4b0914c96b624", delegate: nil)!
         self.api = api
         self.state = .disconnected(.notLogin)
+        self.fileManager = RtmFileManager(api: api)
         super.init()
         self.api.agoraRtmDelegate = self
     }
     
+    //MARK: - call
+
+    public func login(token: String?) {
+        self.api.login(byToken: token, user: self.account) { (code) in
+            
+        }
+    }
+    public func createChannel(id: String) -> RtmChannel? {
+        guard let channel: AgoraRtmChannel = self.api.createChannel(withId: id, delegate: nil) else {
+            return nil
+        }
+        return RtmChannel(kit: self, api: channel)
+    }
+
+    
+    //MARK: - callbacks
     public func rtmKit(_ kit: AgoraRtmKit, peersOnlineStatusChanged onlineStatus: [AgoraRtmPeerOnlineStatus]) {
         
     }
@@ -122,41 +150,178 @@ public final class RtmKit: NSObject, AgoraRtmDelegate {
         assert(kit == self.api, "")
 
     }
+}
+
+
+public final class RtmFile: NSObject {
+    public let digest: String
     
-    public final class Channel: NSObject, AgoraRtmChannelDelegate {
-        private let api: AgoraRtmChannel
-        
-        public init(api: AgoraRtmChannel) {
-            self.api = api
-            super.init()
-            self.api.channelDelegate = self
-        }
-        
-        public func channel(_ channel: AgoraRtmChannel, memberCount count: Int32) {
-            
-        }
-        public func channel(_ channel: AgoraRtmChannel, memberLeft member: AgoraRtmMember) {
-            
-        }
-        public func channel(_ channel: AgoraRtmChannel, memberJoined member: AgoraRtmMember) {
-            
-        }
-        
-        public func channel(_ channel: AgoraRtmChannel, attributeUpdate attributes: [AgoraRtmChannelAttribute]) {
-            
-        }
-        
-        public func channel(_ channel: AgoraRtmChannel, messageReceived message: AgoraRtmMessage, from member: AgoraRtmMember) {
-            
-        }
-        public func channel(_ channel: AgoraRtmChannel, fileMessageReceived message: AgoraRtmFileMessage, from member: AgoraRtmMember) {
-            
-        }
-        public func channel(_ channel: AgoraRtmChannel, imageMessageReceived message: AgoraRtmImageMessage, from member: AgoraRtmMember) {
-            
-        }
+    public var type: Int?
+    public var width: Int?
+    public var height: Int?
+    public var duration: Double?
+    
+    public init(digest: String) {
+        self.digest = digest
     }
+    
+//    public func create
+    
+    
+    /*
+     et://expiration time
+
+     file: {
+     hx: hash
+     tp: type
+     w:
+     h:
+     d: duration
+     }
+     */
+}
+
+
+
+public final class RtmFileManager: NSObject {
+    
+    private let api: AgoraRtmKit
+    
+    fileprivate init(api: AgoraRtmKit) {
+        self.api = api
+    }
+    
+//    public func create
+    
+    
+    //MARK: - call
+    
+//
+//    - (void)createFileMessageByUploading:(NSString * _Nonnull)filePath
+//                      withRequest: (long long*)requestId
+//                      completion:(AgoraRtmUploadFileMediaBlock _Nullable)completionBlock;
+//
+//    /**
+//     Gets an AgoraRtmImageMessage instance by uploading an image to the Agora server.
+//
+//     The SDK returns the result by the [AgoraRtmUploadImageMediaBlock](AgoraRtmUploadImageMediaBlock) callback. If success, this callback returns a corresponding AgoraRtmMessage instance, and then you can downcast it to AgoraRtmImageMessage according to its type.
+//
+//     - If the uploaded image is in JPEG, JPG, BMP, or PNG format, the SDK calculates the width and height of the image. You can get the calculated width and height from the received AgoraRtmImageMessage instance.
+//     - Otherwise, you need to set the width and height of the uploaded image within the received AgoraRtmImageMessage instance by yourself.
+//
+//     **NOTE**
+//
+//     - If you have at hand the media ID of an image on the Agora server, you can call [createImageMessageByMediaId]([AgoraRtmKit createImageMessageByMediaId:]) to create an AgoraRtmImageMessage instance.
+//     - To cancel an ongoing upload task, call [cancelMediaUpload]([AgoraRtmKit cancelMediaUpload:completion:]).
+//
+//     @param filePath The full path to the local image to upload. Must be in UTF-8.
+//     @param requestId The unique ID of the upload request.
+//     @param completionBlock [AgoraRtmUploadImageMediaBlock](AgoraRtmUploadImageMediaBlock) returns the result of this method call. See AgoraRtmUploadMediaErrorCode for the error codes.
+//     */
+//    - (void)createImageMessageByUploading:(NSString * _Nonnull)filePath
+//                      withRequest: (long long*)requestId
+//                      completion:(AgoraRtmUploadImageMediaBlock _Nullable)completionBlock;
+
+    /*
+     et://expiration time
+
+     att: {
+     hx: hash
+     tp: type
+     w:
+     h:
+     d: duration
+     }
+     */
+    
+
+    public typealias ProgressClosure = (Progress) -> Void
+    
+    private var progressMap: [Int64:ProgressClosure] = [:]
 
     
+    private func _upload(filePath: String, progress: ProgressClosure?, completion: @escaping (String) -> Void) {
+        var requestId: Int64 = 0
+
+        self.api.createFileMessage(byUploading: filePath, withRequest: &requestId) { (requestId, fileMessage, code) in
+            print("\(code)")
+        }
+    }
+    public func upload(filePath: String, progress: ProgressClosure?, completion: @escaping (String) -> Void) {
+        if Thread.isMainThread {
+            self._upload(filePath: filePath, progress: progress, completion: completion)
+        } else {
+            DispatchQueue.main.async {
+                self._upload(filePath: filePath, progress: progress, completion: completion)
+            }
+        }
+    }
     
+    public func download(mediaId: String, progress: ProgressClosure?, completion: @escaping (Data) -> Void) {
+        
+        
+    }
+    public func download(mediaId: String, to filePath: String, progress: ProgressClosure?, completion: @escaping (Data) -> Void) {
+                
+    }
+    
+    
+    fileprivate func uploading(kit: AgoraRtmKit, requestId: Int64, progress: AgoraRtmMediaOperationProgress) {
+        assert(kit == self.api, "")
+
+    }
+    fileprivate func downloading(kit: AgoraRtmKit, requestId: Int64, progress: AgoraRtmMediaOperationProgress) {
+        assert(kit == self.api, "")
+
+    }
+    
+}
+
+public final class RtmFileCache: NSObject {
+    private override init() {
+        super.init()
+    }
+
+    public func file(for identifier: String) -> RtmFile? {
+        return nil
+    }
+    
+    public static let shared: RtmFileCache = RtmFileCache()
+}
+
+
+
+public final class RtmChannel: NSObject, AgoraRtmChannelDelegate {
+    private let api: AgoraRtmChannel
+    private let kit: RtmKit
+    fileprivate init(kit: RtmKit, api: AgoraRtmChannel) {
+        self.kit = kit
+        self.api = api
+        super.init()
+        self.api.channelDelegate = self
+    }
+    
+    public func channel(_ channel: AgoraRtmChannel, memberCount count: Int32) {
+        
+    }
+    public func channel(_ channel: AgoraRtmChannel, memberLeft member: AgoraRtmMember) {
+        
+    }
+    public func channel(_ channel: AgoraRtmChannel, memberJoined member: AgoraRtmMember) {
+        
+    }
+    
+    public func channel(_ channel: AgoraRtmChannel, attributeUpdate attributes: [AgoraRtmChannelAttribute]) {
+        
+    }
+    
+    public func channel(_ channel: AgoraRtmChannel, messageReceived message: AgoraRtmMessage, from member: AgoraRtmMember) {
+        
+    }
+    public func channel(_ channel: AgoraRtmChannel, fileMessageReceived message: AgoraRtmFileMessage, from member: AgoraRtmMember) {
+        
+    }
+    public func channel(_ channel: AgoraRtmChannel, imageMessageReceived message: AgoraRtmImageMessage, from member: AgoraRtmMember) {
+        
+    }
 }
